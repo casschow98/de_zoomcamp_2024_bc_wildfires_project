dd269f75e22dd8c536a5eea97296a5d824418b45
Cassidy Chow <cassidychow98@gmail.com>
2024-07-11 18:32:34 +0000

    initial sql test table

--- dbt/models/staging/stg_cchow_table.sql
+++ dbt/models/staging/stg_cchow_table.sql
@@ -1,5 +1,6 @@
 {{ config(materialized='view') }}
 
+{{ config(schema='de_zoomcamp_cchow_dataset') }}
 
 
 with 
@@ -30,4 +31,11 @@ renamed as (
 
 )
 
-select * from renamed;
+select * from renamed
+
+-- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
