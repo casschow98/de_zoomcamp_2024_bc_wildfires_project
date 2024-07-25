173ed2f82c900b7381d4949d9ef570f96ac5c86d
casschow <cassidychow98@gmail.com>
2024-07-25 20:01:36 +0000

    mod project name null values

--- dbt/models/staging/stg_recreation_lines.sql
+++ dbt/models/staging/stg_recreation_lines.sql
@@ -15,8 +15,11 @@ transformed AS (
         CAST(objectid AS INT64) AS Object_ID,
         DATE(TIMESTAMP(PARSE_TIMESTAMP('%Y%m%d', CAST(proj_date AS STRING)))) AS Project_date,
         CAST(rmf_skey AS INT64) AS Feature_code,
-        INITCAP(CAST(project_nm AS STRING)) AS Project_name,
-        DATE(TIMESTAMP(PARSE_TIMESTAMP('%Y%m%d%H%M%S', CAST(retire_dt AS STRING)))) AS Retire_date ,
+        COALESCE(
+            INITCAP(CAST(project_nm AS STRING)),
+            CONCAT('FFID: ',CAST(ffid AS STRING))
+        ) AS Project_name,
+        DATE(TIMESTAMP(PARSE_TIMESTAMP('%Y%m%d%H%M%S', CAST(retire_dt AS STRING)))) AS Retire_date,
         CAST(feat_len AS FLOAT64) AS Feature_length_m,
         INITCAP(CAST(site_loc AS STRING)) AS Site_location,
         CAST(def_camps AS INT64) AS Campsites,
