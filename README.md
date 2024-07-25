9a563bc6d9528866cb04cfc36e92e5cd02514fc7
casschow <cassidychow98@gmail.com>
2024-07-25 06:10:56 +0000

    Remove ignored files from tracking

--- README.md
+++
@@ -1,2 +0,0 @@
-# de_zoomcamp_2024_bc_wildfires_project
-As part of DataTalksClub Data Engineering Zoomcamp, this project marks the final component of the course.
--- airflow/scripts/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-airflow db init
-airlfow db upgrade
-airflow users create -r Admin -u admin -p admin -e admin@example.com -f admin -l airflow
-airflow scheduler &
-airflow webserver
